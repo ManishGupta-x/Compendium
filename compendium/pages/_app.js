e1c7e0c4fb4e873e008061803ce779a553061530
Manish Gupta <63774978+ManishGupta-x@users.noreply.github.com>
2024-09-19 20:26:14 +0530

    Update _app.js

--- compendium/pages/_app.js
+++ compendium/pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "@/styles/globals.css";
-import MobileAlert from '@/components/MobileAlert';
+import MobileAlert from '@/components/ui/MobileAlert';
 
 export default function App({ Component, pageProps }) {
   return (
